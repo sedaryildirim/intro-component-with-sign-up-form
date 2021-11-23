9f406dfbdb698bf9f16c37460ad65d94188ba796
Sedar Yildirim <40918409+sedaryildirim@users.noreply.github.com>
2021-11-23 21:35:00 +0000

    index, js and scss updated
    added all error states in html and scss, started js form validation

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const emailAddress = document.getElementById('email-address');
+const passsword = document.getElementById('password');
+
